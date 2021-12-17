d207bec2aa936d9d7a98c0d9d4318f0979c24917
Ramírez Moreno Diego Gerardo <90343027+252003@users.noreply.github.com>
2021-12-17 11:18:43 -0600

    Update polinomio.h

--- polinomio.h
+++ polinomio.h
@@ -5,14 +5,18 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include<ctype.h>
-//Fecha de Creacion: 09/12/2021
-//Autores:Ramirez Moreno Diego Gerardo
 
+struct cadenas
+{
+    int cont; 
+    char **cdn;
+};
+typedef struct cadenas CADENAS;
+CADENAS almacen;
 struct _termino
 {
     float coef;
     int exp;
-    //char variable;
 };
 typedef struct _termino TERMINO;
 struct polinomio
@@ -24,48 +28,58 @@ struct polinomio
 typedef struct polinomio POL;
 struct _mpol
 {
-    int cont; //Indice en la que vamos a guardar el polinomio
+    int cont; 
     POL *polis; 
 };
 typedef struct _mpol mem; 
 mem memoria;
+struct division
+{
+    POL cociente;
+    POL residuo;
+};
+typedef struct division DIV;
 
+POL pol_creat(int nter);
+void pol_del(POL A);
+POL* initmemory();
+void cpyinmem(POL A);
+void cpy_in_almacen(char*);
+void printMemory(mem);
+POL recoMemory(mem,int);
+void cdnprint();
+char* POLtoSTR(POL);
 
-//-------ARCHIVOS---------
-FILE *infile;
-FILE *outfile;
-void open_in_file(char *);
-void open_out_file(char*);
-void close_file(FILE *f);
-POL fpol_creat(char*);
-//-------AUXILIARES--------
 int pot(int base, int expo);
-int findstrc(char*str,char c);
 int strtoi(char *str);
 char*strcpyrng(char*,int a,int b);
 bool find(char*,char);
 int findstrc(char *str, char c);
 void menu();
 void init();
-//-------OPERACIONES-------
-double residuo(POL,double);
+
+FILE *infile;
+FILE *outfile;
+FILE *registro;
+void open_in_file(char *);
+void open_out_file(char*);
+void open_registro(char*);
+void close_file(FILE *f);
+POL fread_pol(FILE*);
+void fwrite_pol(int);
+
+POL pol_cero(POL);
 POL add(POL,POL);
 POL sub(POL,POL);
 POL esc_pol(POL,double);
 POL pol_prod(POL,POL);
 POL pol_div(POL,POL);
+DIV div_larga(POL,POL);
 POL polcpy(POL);
-void cpyinmem(POL);
-//-------MEMORIA---------
-POL pol_creat(int nter);
-void pol_del(POL A);
-POL* initmemory();
-void printMemory(mem);
-POL recoMemory(mem,int);
-//------ESCRITURA-------
+POL cpy(POL);
+TERMINO t_read(char *);
+
 void pol_print(POL A);
-TERMINO read_ter(char*a);
-//-----ORDENAMIENTO-----
 void bubble_sort(int a[], int n);
 void bubble_pol(POL);
 #endif
